f928aaf2efb6f2d86f1a4bf6801a93434e0c80e9
Lounes Adjou <lmatoub91@gmail.com>
2023-04-25 18:40:43 -0400

    speed increase + norminette

--- src/utils/moves.c
+++ src/utils/moves.c
@@ -6,7 +6,7 @@
 /*   By: loadjou <loadjou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:24:10 by loadjou           #+#    #+#             */
-/*   Updated: 2023/04/25 18:37:08 by loadjou          ###   ########.fr       */
+/*   Updated: 2023/04/25 18:39:14 by loadjou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ static void	adjust(t_cub *cub, float dx, float dy, unsigned char flag)
 	dy *= 4;
 	if (flag == 'a')
 	{
-		if (!is_wall(cub, cub->coord[X] + cub->pdx * 4, cub->coord[Y] + cub->pdy * 4))
+		if (!is_wall(cub, cub->coord[X] + cub->pdx * 4, cub->coord[Y] + cub->pdy
+				* 4))
 		{
 			cub->coord[X] += dx;
 			cub->coord[Y] += dy;
@@ -26,7 +27,8 @@ static void	adjust(t_cub *cub, float dx, float dy, unsigned char flag)
 	}
 	if (flag == 's')
 	{
-		if (!is_wall(cub, cub->coord[X] - cub->pdx * 4, cub->coord[Y] - cub->pdy * 4))
+		if (!is_wall(cub, cub->coord[X] - cub->pdx * 4, cub->coord[Y] - cub->pdy
+				* 4))
 		{
 			cub->coord[X] -= dx;
 			cub->coord[Y] -= dy;
